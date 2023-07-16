842c92676504860fd916b5094c12ddf0ce6e1df3
johndev07 <alwynjohn.dev@gmail.com>
2023-07-16 20:22:04 +0530

    started cars hub project

---
+++ components/Hero.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          find book or rent a car --quickly and easily
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our efortless booking
+          process
+        </p>
+        {/* <CustomButton/> */}
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
